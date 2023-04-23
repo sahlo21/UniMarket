140a255eb0cb2b17215300aa69c01d2e1a5803b2
sahlo21 <kssm21p@gmail.com>
2023-04-22 22:28:11 -0500

    dataset v1

--- src/main/java/co/edu/uniquindio/unimarket/modelo/dto/UsuarioDTO.java
+++ src/main/java/co/edu/uniquindio/unimarket/modelo/dto/UsuarioDTO.java
@@ -14,26 +14,26 @@ public class UsuarioDTO {
 
     @NotNull
     @NotBlank
-    @Length(max = 150, message = "El nombre debe tener mÃ¡ximo 100 caracteres")
+    @Length(max = 150, message = "El nombre debe tener maximo 100 caracteres")
     private String nombre;
 
     @NotNull
     @NotBlank
-    @Length(max = 100, message = "El correo debe tener mÃ¡ximo 100 caracteres")
+    @Length(max = 100, message = "El correo debe tener maximo 100 caracteres")
     private String email;
 
     @NotNull
     @NotBlank
-    @Length(max = 50, message = "La contraseÃ±a debe tener mÃ¡ximo 50 caracteres")
+    @Length(max = 50, message = "La contrasena debe tener maximo 50 caracteres")
     private String contrasena;
 
     @NotNull
     @NotBlank
-    @Length(max = 100, message = "La direcciÃ³n debe tener mÃ¡ximo 100 caracteres")
+    @Length(max = 100, message = "La direccion debe tener maximo 100 caracteres")
     private String direccion;
 
     @NotNull
     @NotBlank
-    @Length(max = 12, message = "El telÃ©fono debe tener mÃ¡ximo 12 caracteres")
+    @Length(max = 12, message = "El telefono debe tener maximo 12 caracteres")
     private String telefono;
 }
---
+++ src/test/resources/dataset.sql
@@ -0,0 +1,2 @@
+insert into cliente values (1, "pepito@gmail.com", "Pepito", "brocoli", "Calle 11 # 2 - 33",
+"7506789");
